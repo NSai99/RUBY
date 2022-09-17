d1beb608074bbe5c33e9577ff550d9383f5eece3
Narasimha Sai Addepalli <anarasimhasai2407@gmail.com>
2022-09-17 17:38:13 +0530

    section1

--- Ruby/Helloworld.rb
+++ Ruby/Helloworld.rb
@@ -1,2 +1,20 @@
 puts "Hello World"
 print("Hello World")
+p "Hello World"
+p 12/5.0
+
+# comments
+
+# single line comments 
+
+=begin
+multi line comments
+are used     
+=end
+
+a = 10
+b = 20
+# print a, b
+puts "this is #$a and #$b"
+a , b  = b, a
+print a, b
---
+++ Ruby/object_methods.rb
@@ -0,0 +1,5 @@
+puts "Hello World".length
+puts 20.next
+puts 'a'.next
+puts "Hello World".upcase
+puts "Hello World".downcase
